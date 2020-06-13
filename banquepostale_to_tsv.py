d54c638d244d24f7ffbce1750967dcfa8903552c
Olivier Aubert <contact@olivieraubert.net>
2020-06-13 23:02:49 +0200

    Fix regexps

--- banquepostale_to_tsv.py
+++ banquepostale_to_tsv.py
@@ -54,7 +54,7 @@ def data_lines(lines):
             reference_line = l
             continue
 
-        m = re.search(r'Relevé édité le (?P<day>\d\d) (?P<month>\w+) (?P<year>\d\d\d\d)', l)
+        m = re.search(r'Relevé\s+édité\s+le\s+(?P<day>\d\d)\s+(?P<month>\w+)\s+(?P<year>\d\d\d\d)', l)
         if m:
             # In this order so that we can compare date tuples
             publication_date = ( int(m.group('year')),
@@ -62,12 +62,12 @@ def data_lines(lines):
                                  int(m.group('day')) )
             continue
 
-        m = re.search('.+n° (?P<account>[\w\d ]+)\s*', l)
+        m = re.search('.+n°\s+(?P<account>[\w\d ]+)\s*', l)
         if m:
             current_account = m.group('account').replace(' ', '')
             continue
 
-        m = re.search('^\s+(?P<label>(Ancien|Nouveau) solde)\sau\s+(?P<date>\d{2}/\d{2}/\d{4})\s+(?P<value>\d{,3}(?: \d{3})*(?:,\d+)?)\s*$', l)
+        m = re.search('^\s+(?P<label>(Ancien|Nouveau)\s+solde)\s+au\s+(?P<date>\d{2}/\d{2}/\d{4})\s+(?P<value>\d{,3}(?: \d{3})*(?:,\d+)?)\s*$', l)
         if m:
             if current_record is not None:
                 yield current_record
@@ -83,21 +83,17 @@ def data_lines(lines):
             if publication_date < (2017, 3, 1):
                 # Before 1st march 2017, there is an extra column with the price in
                 # francs
-                m = re.match(r'\s*(?P<day>\d{2})\/(?P<month>\d{2})(?P<title>.*?)(?P<cents>\d{,3}(?: \d{3})*(,\d+)?) +(<?Pfrancs>(?:-|\+ )\d{,3}(?: \d{3})*(?:,\d+)?)$', l)
+                m = re.match(r'\s*(?P<day>\d{2})\/(?P<month>\d{2})(?P<title>.*?)(?P<value>\d{,3}(?: \d{3})*(,\d+)?)\s+(?P<francs>(?:-|\+ )\d{,3}(?: \d{3})*(?:,\d+)?)\s*$', l)
                 value = float(m.group('value').replace(' ', '').replace(',', '.'))
                 value = -value if m.group('francs')[0] == '-' else value
-                date = f"{m.group('month')}/{m.group('day')}"
-                title = m.group('title')
             else:
-                m = re.search('^\s*(?P<date>\d{2}/\d{2})\s+(?P<title>.+?)\s+(?P<value>\d{,3}(?: \d{3})*(?:,\d+)?)\s*$', l)
+                m = re.search('^\s*(?P<day>\d{2})/(?P<month>\d{2})\s+(?P<title>.+?)\s+(?P<value>\d{,3}(?: \d{3})*(?:,\d+)?)\s*$', l)
                 amount = float(m.group('value').replace(" ", "").replace(",", "."))
                 if len(l) < len(reference_line) - 12:
                     amount = -amount
-                # Add year
-                date = m.group('date')
-                date = f"{date[-2:]}/{date[:2]}"
-                title = m.group('title')
 
+            date = f"{m.group('month')}/{m.group('day')}"
+            title = m.group('title')
             year = publication_date[0]
             if date[:2] == "12" and publication_date[1] == 1:
                 year = year - 1
